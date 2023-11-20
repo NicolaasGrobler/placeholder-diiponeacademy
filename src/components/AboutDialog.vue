afbffe5bdbc7697834ce3f46134923af716cbe0e
Nicolaas Grobler <nicolaasgrob@gmail.com>
2023-11-20 15:00:59 +0200

    Update AboutDialog.vue

--- src/components/AboutDialog.vue
+++ src/components/AboutDialog.vue
@@ -9,8 +9,8 @@
                 <div class="fixed inset-0 bg-black/80" />
             </TransitionChild>
 
-            <div class="fixed inset-0 overflow-y-auto">
-                <div class="flex min-h-full items-center justify-center p-4 text-center">
+            <div class="fixed inset-0 w-screen overflow-y-auto">
+                <div class="flex min-h-full items-center justify-center p-4">
                     <TransitionChild as="template" enter="duration-300 ease-out" enter-from="opacity-0 scale-95"
                         enter-to="opacity-100 scale-100" leave="duration-200 ease-in" leave-from="opacity-100 scale-100"
                         leave-to="opacity-0 scale-95">
